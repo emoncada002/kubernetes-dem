0b8982e26342aef9d3b129f638bbac025186d348
DariusAlvram <darioalvarezram@gmail.com>
2026-08-17 21:34:11 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+  # Obtiene el nombre del Pod desde la variable de entorno HOSTNAME (por defecto en Kubernetes)
+  # o utiliza "localhost" si se ejecuta fuera de un entorno K8s.
+  pod_name = os.environ.get("HOSTNAME", "localhost")
+  return f"Hola desde Kubernetes - Atendido por el Pod: {pod_name}"
+
+
+@app.route("/health")
+def health():
+  return "OK", 200
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
